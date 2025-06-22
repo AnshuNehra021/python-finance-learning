4697173317e05102170f18e51c0c652782a6ece2
AnshuNehra021 <AnshuNehra021@gmail.com>
2025-06-22 18:21:01 +0530

    added day 2 assignment

--- day2_variables_and_dataTypes.py
+++ day2_variables_and_dataTypes.py
@@ -1,8 +1,10 @@
-Income = input("Monthly income: ")
-Expenses = input("Monthly expenses:" )
+Income = input("Monthly income: Rs. ")
+Expenses = input("Monthly expenses: Rs. " )
 savings = float(Income) - float(Expenses)
-print("Monthly Savings: " + str(savings))
+print("Monthly Savings: Rs. " + str(round(savings,0)))
 if savings > 20000:
     print("Great! you are saving well.")
 elif savings <5000:
-    print("Watch your spending")
+    print("Watch your spending.")
+else:
+    print("You are doing well.")
