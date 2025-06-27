b0038c4d08da74fd75190d1e4e0530a6bfddfa51
AnshuNehra021 <AnshuNehra021@gmail.com>
2025-06-27 16:13:06 +0530

    added day 2 mini challenge

---
+++ day4_functions.py
@@ -0,0 +1,15 @@
+income = float(input("Monthly income: "))
+expenses = float(input("Monthly expenses: "))
+def calculation_saving (income, expenses):
+     return income - expenses
+saving = calculation_saving(income, expenses)
+print(f"Monthly saving: {saving}")
+def saving_percentage (saving, income):
+    return saving / income * 100
+saving_percentage = saving_percentage (saving, income)
+if saving_percentage > 35:
+    print("Well done! Great savings")
+elif saving_percentage > 10:
+    print("You are doing well")
+else:
+    print("Check your expenses")
